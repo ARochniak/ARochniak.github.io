36fc3585d02cb2d08f73eece2f6a668493c2f917
ARochniak <15bblinn@gmail.com>
2019-12-15 20:57:34 +0200

    Links images convert

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const projects = [
   {
-    title: 'asteroids_game',
-    imageSrc: 'images/asteroids.jpg',
-    href: 'https://arochniak.github.io/AsteroidsGame/'
+    title: "asteroids_game",
+    imageSrc: "images/asteroids.webp",
+    href: "https://arochniak.github.io/AsteroidsGame/"
   },
   {
-    title: 'calculator',
-    imageSrc: 'images/calculator.jpg',
-    href: 'https://arochniak.github.io/React-Calculator/' 
+    title: "calculator",
+    imageSrc: "images/calculator.webp",
+    href: "https://arochniak.github.io/React-Calculator/"
   },
   {
-    title: 'weather app',
-    imageSrc: 'images/weatherApp.png',
-    href: 'https://arochniak.github.io/weatherapp/'
+    title: "weather app",
+    imageSrc: "images/weatherApp.webp",
+    href: "https://arochniak.github.io/weatherapp/"
   },
   {
-    title: 'notes',
-    imageSrc: 'images/ARNotes.png',
-    href: 'https://arochniak.github.io/ARNotes/'
+    title: "notes",
+    imageSrc: "images/ARNotes.webp",
+    href: "https://arochniak.github.io/ARNotes/"
   },
   {
-    title: 'simple store',
-    imageSrc: 'images/simpleStore.png',
-    href: 'https://arochniak.github.io/simpleStore/'
+    title: "simple store",
+    imageSrc: "images/simpleStore.webp",
+    href: "https://arochniak.github.io/simpleStore/"
   },
   {
-    title: 'orders manager',
-    imageSrc: 'images/orders.png',
-    href: 'https://arochniak.github.io/orders_products/'
+    title: "orders manager",
+    imageSrc: "images/orders.webp",
+    href: "https://arochniak.github.io/orders_products/"
   }
 ];
 
-const works = document.createElement('div');
-works.classList.add('works');
+const works = document.createElement("div");
+works.classList.add("works");
 
-projects.forEach( project => {
-  const figure = document.createElement('figure');
-  const a = document.createElement('a');
-  const img = document.createElement('img');
-  const figcaption = document.createElement('figcaption');
+projects.forEach(project => {
+  const figure = document.createElement("figure");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
   a.href = project.href;
   img.src = project.imageSrc;
   img.alt = project.title;
@@ -47,6 +47,6 @@ projects.forEach( project => {
   figcaption.textContent = project.title;
   figure.append(figcaption);
   works.append(figure);
-})
+});
 
-document.getElementsByTagName('main')[0].append(works);
+document.getElementsByTagName("main")[0].append(works);
